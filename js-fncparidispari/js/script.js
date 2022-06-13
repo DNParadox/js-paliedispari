7f9fd0a2e0f605e0da0fcea086c9f06948b0104b
DNParadox <m96francesco@gmail.com>
2022-06-13 15:37:29 +0200

    Codice completo

--- js-fncparidispari/js/script.js
+++ js-fncparidispari/js/script.js
@@ -9,12 +9,12 @@
 // facciamo dichiarare all'utente il suo numero da 1 a 5 e se sceglierà "Pari o Dispari"
 const userNumber = parseInt(prompt (' Dammi un numero da 1 a 5'));
 const userChoice = prompt ('Scegli Pari o Dispari').toLowerCase();
-console.log (userChoice)
-console.log('numero casuale utente', userNumber)
+console.log ("La scelta dell'utente è " , userChoice)
+console.log("Il numero scelto dall'utente è ", userNumber)
 
 // Genererà un numero da 1 a 5 attraverso una function
 const pcNumberChoice = pcNumber(1, 5)
-console.log('numero casuale pc', pcNumberChoice)
+console.log('Il numero del computer è ', pcNumberChoice)
 
 
 // Funzione che dichiara un numero da 1 a 5 da parte del Computer
@@ -25,21 +25,32 @@ function pcNumber (min,max) {
 
 // Sommiamo i due numeri
 const totalsum = userNumber + pcNumberChoice;
-console.log ('somma totale', totalsum)
+console.log ('La somma dei due numeri è ', totalsum)
 
 
-let pcChoicetest = pcEvenOrOdd (pcChoice)
-console.log(pcChoicetest)
-// Funzione dichiarativa della scelta del pc
+const pcChoicetest = pcEvenOrOdd ('pari' , 'dispari')
+console.log('scelta del pc', pcChoicetest)
+// Funzione dichiarativa della scelta del pc pari o dispari
 function pcEvenOrOdd (pcChoice) {
     // Dovrà dichiarare se il pc sceglie pari o dispari
+    // SE userChoice equivale a "Pari " allora il pc sceglie "Dispari" altrimenti viceversa
     if (userChoice === 'pari' ) {
-        pcChoice  = 'dispari'
-        console.log(pcChoice)
+        return pcChoice  = 'Dispari'
     } else if (userChoice === 'dispari') {
-        return pcChoice = 'dispari'    
+        return pcChoice = 'Pari'    
     } 
+}
 
+
+const evenOrOdd = declarationResult ('pari', 'dispari')
+console.log('Il vincitore è ',evenOrOdd)
+function declarationResult (declaration) {
+    if (totalsum % 2 === 0) {
+        return declaration = 'Pari'
+    } else {
+        return declaration = 'Dispari'
+    }
+    
 }
 
 
